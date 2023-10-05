1ee3605c2a18079ad016c3fd3a5dcc84dae89787
khalilannbiya <syeichkhalil@gmail.com>
2023-10-05 19:51:34 +0700

    feat(server): create server

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import Hapi from "@hapi/hapi";
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: "localhost",
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
