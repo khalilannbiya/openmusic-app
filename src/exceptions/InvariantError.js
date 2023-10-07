5ab4561536cf11581ba7499e39a537f9bd5c3e11
khalilannbiya <syeichkhalil@gmail.com>
2023-10-07 09:26:04 +0700

    feat: create custom error

---
+++ src/exceptions/InvariantError.js
@@ -0,0 +1,11 @@
+import ClientError from "./ClientError.js";
+
+class InvariantError extends ClientError {
+  constructor(message) {
+    super(message);
+
+    this.name = "InvariantError";
+  }
+}
+
+export default InvariantError;
