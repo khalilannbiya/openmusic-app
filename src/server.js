ca9496fc83b232c5675e2ae4b6c77d43f20a78ce
khalilannbiya <syeichkhalil@gmail.com>
2023-10-09 11:06:07 +0700

    fix:change AlbumService in server. import to require

--- src/server.js
+++ src/server.js
@@ -1,14 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// import Hapi from "@hapi/hapi";
-// import albums from "./api/albums/index.js";
-// import songs from "./api/songs/index.js";
-// import AlbumsService from "./services/inMemory/AlbumsService.js";
-// import SongsService from "./services/inMemory/SongsService.js";
-// import AlbumsValidator from "./validator/albums/index.js";
-// import SongsValidator from "./validator/songs/index.js";
-
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -16,7 +5,7 @@ const Hapi = require("@hapi/hapi");
 
 const albums = require("./api/albums/index.js");
 const songs = require("./api/songs/index.js");
-const AlbumsService = require("./services/inMemory/AlbumsService.js");
+const AlbumsService = require("./services/postgres/AlbumService.js");
 const SongsService = require("./services/inMemory/SongsService.js");
 const AlbumsValidator = require("./validator/albums/index.js");
 const SongsValidator = require("./validator/songs/index.js");
