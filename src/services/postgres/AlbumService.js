301523735d3251c84eb471a699ce223423057496
khalilannbiya <syeichkhalil@gmail.com>
2023-10-09 10:20:29 +0700

    feat:create fun addAlbum use service postgres

---
+++ src/services/postgres/AlbumService.js
@@ -0,0 +1,22 @@
+const { Pool } = require("pg");
+
+class AlbumService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async addAlbum({ name, year }) {
+    const id = `album-${nanoid(16)}`;
+
+    const query = {
+      text: "INSERT INTO albums VALUES($1, $2, $3) RETURNING id",
+      values: [id, name, year],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) throw new InvariantError("Album gagal ditambahkan!");
+
+    return result.rows[0].id;
+  }
+}
