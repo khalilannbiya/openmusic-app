acdd5970260e533a4e01ad24a93b9c9788a7a3da
khalilannbiya <syeichkhalil@gmail.com>
2023-10-19 19:35:55 +0700

    feat: get playlist by id

--- src/api/playlists/handler.js
+++ src/api/playlists/handler.js
@@ -45,6 +45,38 @@ class PlaylistsHandler {
       return response;
     }
   }
+
+  async getPlaylistsHandler(request, h) {
+    try {
+      const { id: credentialId } = request.auth.credentials;
+
+      const playlists = await this._service.getPlaylists(credentialId);
+      return {
+        status: "success",
+        data: {
+          playlists,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server Error
+      const response = h.response({
+        status: "Error",
+        message: "Maaf, terjadi kegagalan pada server kami!",
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
 }
 
 export default PlaylistsHandler;
--- src/services/postgres/PlaylistsService.js
+++ src/services/postgres/PlaylistsService.js
@@ -9,12 +9,12 @@ class PlaylistsService {
     this._pool = new Pool();
   }
 
-  async addPlaylist(credentialId, { name }) {
+  async addPlaylist(owner, { name }) {
     const id = `playlist-${nanoid(16)}`;
 
     const query = {
       text: "INSERT INTO playlists VALUES($1, $2, $3) RETURNING id",
-      values: [id, credentialId, name],
+      values: [id, owner, name],
     };
 
     const { rows } = await this._pool.query(query);
@@ -23,6 +23,29 @@ class PlaylistsService {
 
     return rows[0].id;
   }
+
+  async getPlaylists(owner) {
+    const query = {
+      text: "SELECT playlists.id, playlists.name, users.username FROM playlists LEFT JOIN users ON playlists.owner = users.id WHERE playlists.owner = $1",
+      values: [owner],
+    };
+    const { rows } = await this._pool.query(query);
+    return rows;
+  }
+
+  // TODO: aktifkan ketika ingin digunakan untuk verifikasi owner
+  // async verifyPlaylistOwner(id, owner) {
+  //   const query = {
+  //     text: "SELECT * FROM playlists WHERE id = $1",
+  //     values: [id],
+  //   };
+
+  //   const { rows } = await this._pool.query(query);
+  //   if (!rows.length) throw new NotFoundError("Playlist tidak ditemukan");
+
+  //   const playlist = rows[0];
+  //   if (playlist.owner !== owner) throw new AuthorizationError("Anda tidak berhak mengakses resource ini");
+  // }
 }
 
 export default PlaylistsService;
